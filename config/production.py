0cc3a8c4334851e52ed8f8bb321be1ae68172141
keunbum <rkeunbum@gmail.com>
2022-10-19 19:31:46 +0900

    modify __init__.py

--- config/production.py
+++ config/production.py
@@ -7,7 +7,6 @@ SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://{user}:{pw}@{url}/{db}'.format(
     user=os.getenv('DB_USER'),
     pw=os.getenv('DB_PASSWORD'),
     url=os.getenv('DB_HOST'),
-    db=os.getenv('DB_NAME'),
-)
+    db=os.getenv('DB_NAME'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xd5|U\xa7\xe8X\x07\xd1\x98\xf5w\x8c\xda\xb7p4'
