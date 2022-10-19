e74bd2b518d0d369ef2a62a60ca923e1ba3221ae
keunbum <rkeunbum@gmail.com>
2022-10-19 18:36:23 +0900

    update production.py

--- config/production.py
+++ config/production.py
@@ -1,5 +1,13 @@
 from config.default import *
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'),
+)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xd5|U\xa7\xe8X\x07\xd1\x98\xf5w\x8c\xda\xb7p4'
