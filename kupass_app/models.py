2ae1ac5ecee97dbed08e4500568b4340ce5016f3
keunbum <rkeunbum@gmail.com>
2022-10-19 20:11:02 +0900

    update models.py

--- kupass_app/models.py
+++ kupass_app/models.py
@@ -2,17 +2,22 @@ from kupass_app import db
 
 
 class Article(db.Model):
-    article_id = db.Column(db.BigInteger, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     category = db.Column(db.String(255), nullable=True)
-    content = db.Column(db.String(255), nullable=True)
+    content = db.Column(db.String(255), nullable=False)
     create_date = db.Column(db.DateTime(6), nullable=True)
     publisher = db.Column(db.String(255), nullable=True)
     source = db.Column(db.String(255), nullable=True)
     summary = db.Column(db.String(255), nullable=True)
-    title = db.Column(db.String(255), nullable=True)
+    title = db.Column(db.String(255), nullable=False)
 
 class Keyword(db.Model):
-    keyword_id = db.Column(db.BigInteger, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     keyword = db.Column(db.String(255), nullable=True)
-    article_id = db.Column(db.BigInteger, nullable=True)
+
+class User(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    activated = db.Column(db.Boolean, nullable=True)
+    nickname = db.Column(db.String(255), nullable=True)
+    password = db.Column(db.String(255), nullable=True)
 
